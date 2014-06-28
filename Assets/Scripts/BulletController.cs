83359c49beadf1c940689eb5468601ef1290ba65
MdotIvanov <MdotIvanov@users.noreply.github.com>
2014-06-29 02:21:17 +0300

    boundery

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -16,4 +16,10 @@ public class BulletController : MonoBehaviour {
 	void Update () {
 	
 	}
+	
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject.tag != "Player" ){
+			Destroy (gameObject);
+		}
+	}
 }
