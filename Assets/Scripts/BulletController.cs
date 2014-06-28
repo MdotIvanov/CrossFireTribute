0178e457b975f14dafb2be4b82cab6c3003299b8
dilyanrusev <dilyanrusev@gmail.com>
2014-06-29 01:36:14 +0300

    bullets move in proper direction

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -5,7 +5,11 @@ public class BulletController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		rigidbody2D.AddForce (Vector2.up * 100);
+		//var dir = transform.rotation * Vector3.forward;
+		//var dir2d = new Vector2 (dir.x, dir.y);
+		//dir2d.Normalize ();
+		var dir = transform.rotation * Vector2.up;
+		rigidbody2D.AddForce (dir * 100);
 	}
 	
 	// Update is called once per frame
