e9ee23103362a89a1acf7b1c9f9d48c20dac7234
dilyanrusev <dilyanrusev@gmail.com>
2014-06-29 01:21:22 +0300

    added movement & shooting rockets

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.AddForce (Vector2.up);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -5,12 +5,27 @@ public class HeroController : MonoBehaviour {
 
 	//public float force; 
 	public float speed; 
+	public Transform spawnUp, spawnDown, spawnLeft, spawnRight;
+	public GameObject bulletPrefab;
+
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	void Update() {
+		if (Input.GetButtonDown("FireRight")) {
+			Instantiate(bulletPrefab, spawnRight.position, spawnRight.rotation);
+		} else if (Input.GetButtonDown("FireLeft")) {
+			Instantiate(bulletPrefab, spawnLeft.position, spawnLeft.rotation);
+		} else if (Input.GetButtonDown("FireUp")) {
+			Instantiate(bulletPrefab, spawnUp.position, spawnUp.rotation);
+		} else if (Input.GetButtonDown("FireDown")) {
+			Instantiate(bulletPrefab, spawnDown.position, spawnDown.rotation);
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
