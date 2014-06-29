d8ab2526da368c02d6d30d85d5f7ca12853b9ff5
dilyanrusev <dilyanrusev@gmail.com>
2014-06-29 05:40:30 +0300

    trying to make enemies die when shot AND hero die when he touches them

---
+++ Assets/Scripts/EnemyControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyControllerBase : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collision2D collision) {
+		if (collider.gameObject.tag == "Ammo") {
+			Destroy(gameObject);
+		} else if (collider.gameObject.tag == "Player") {
+			Destroy (collider.gameObject);
+		}
+	}
+}
