5051fdc2dc6ebe39ce80327c8b03c17661148d94
dilyanrusev <dilyanrusev@gmail.com>
2014-06-29 05:26:23 +0300

    basic enemy spawning

---
+++ Assets/Scripts/MasterSpawnerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterSpawnerController : MonoBehaviour {
+	private GameObject[] spawnPositions;
+	public GameObject[] enemies;
+	public float minWait = 1;
+	public float maxWait = 3;
+
+	// Use this for initialization
+	void Start () {
+		spawnPositions = GameObject.FindGameObjectsWithTag ("Respawn");
+
+		StartCoroutine (SpawnNext());
+	}
+
+	IEnumerator SpawnNext() {
+		while (gameObject) {
+			int nextSpawnPoint = Random.Range (0, spawnPositions.Length);
+			int nextEnemy = Random.Range (0, enemies.Length);
+
+			Instantiate (enemies [nextEnemy], 
+			            spawnPositions [nextSpawnPoint].transform.position,
+			            spawnPositions [nextSpawnPoint].transform.rotation);
+
+			yield return new WaitForSeconds (Random.Range (minWait, maxWait));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
